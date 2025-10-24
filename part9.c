4cb4a88b81f52bde000c72c47cd8703c8ee47bc4
gocchi <shogo@gocchi.localdomain>
2025-10-24 13:03:30 +0900

    add new files

---
+++ part9.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char alfabet[28] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', '\n'};
+
+char create(int *mem, int random, int *ind, int *ind2, int *ind3)
+{
+    int border = 0;
+    int i = 0;
+    while (i < 28)
+    {
+        border += mem[i];
+        if (random < border)
+        {
+            *ind = *ind2;
+            *ind2 = *ind3;
+            *ind3 = i;
+            return alfabet[i];
+        }
+        i++;
+    }
+}
+
+int main(void)
+{
+
+    srand((unsigned int)time(NULL));
+
+    FILE *fp;
+    int counts[28][28][28][28];
+    char gomi[5] = {
+        '\0',
+    };
+
+    fp = fopen("part8.csv", "r");
+    if (fp == NULL)
+    {
+        printf("ERROR");
+        return 1;
+    }
+    char str[64];
+
+    int sums[28][28][28] = {
+        {
+            {
+                0,
+            },
+        },
+    };
+
+    for (int i = 0; i < 28; i++)
+        for (int j = 0; j < 28; j++)
+            for (int k = 0; k < 28; k++)
+                for (int l = 0; l < 28; l++)
+                {
+                    fgets(str, sizeof(str), fp);
+                    sscanf(str, "%c%c%c%c,%d", &gomi[0], &gomi[1], &gomi[2], &gomi[3], &counts[i][j][k][l]);
+                    sums[i][j][k] += counts[i][j][k][l];
+                }
+
+    int ind1 = 26;
+    int ind2 = 26;
+    int ind3 = 26;
+
+    char passage[1000];
+    for (int i = 0; i < 999; i++)
+    {
+        passage[i] = create(counts[ind1][ind2][ind3], rand() % sums[ind1][ind2][ind3] + 1, &ind1, &ind2, &ind3);
+    }
+    passage[999] = '\0';
+
+    printf("%s\n", passage);
+    fclose(fp);
+
+    return 0;
+}
